312405e40f16b6b4c4f9cfc62468b8462624f442
Deyvid <>
2021-11-25 06:39:46 +0000

    02.01

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -9,6 +9,7 @@ public class Triangle {
         System.out.print("Enter n: ");
         int n = input.nextInt();
 
+        System.out.println();
         for (int line = 1; line <= n*2; line++) {
           if(line > n) {
             printLine(1, n*2 - line);
@@ -21,7 +22,7 @@ public class Triangle {
 
     public static void printLine(int start, int end) {
       for(int i = 1; i <= end; i++) {
-        System.out.print(i);
+        System.out.print(i + " ");
       }
       System.out.println();
     }
